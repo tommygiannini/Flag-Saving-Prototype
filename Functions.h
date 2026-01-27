35380bd7ca7fed4c649d7c8dd4a8269f66122816
Thomas Giannini <thomasgiannini42@outlook.com>
2026-01-27 00:04:16 -0600

    added function readLineFromFile
    added function readLineFromFile to branch adding-getLine

--- Functions.h
+++ Functions.h
@@ -1,1 +1,8 @@
-#pragma once
+#ifndef FUNCTIONS
+#define FUNCTIONS
+
+void changeInFile(std::string fileLocation, int lineToChange, std::string newData);
+
+std::string readLineFromFile(std::string fileLocation, int lineToRead);
+
+#endif FUNCTIONS
